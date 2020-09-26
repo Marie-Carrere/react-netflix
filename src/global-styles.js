de0517ff02d802ea713173bf9bb36081091a8c17
Marie-Carrere <marie.carrere@icloud.com>
2020-09-26 10:11:43 +0200

    :lipstick: Add global style

---
+++ src/global-styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle} from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialised;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: black;
+    color: #333333;
+    font-size: 16px;
+  }
+`
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "normalize.css"
 import App from './App';
+import { GlobalStyles } from "./global-styles"
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
