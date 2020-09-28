9b8b3ff72226330906591646ae583c6dd11f7b7e
Marie-Carrere <marie.carrere@icloud.com>
2020-09-28 12:35:35 +0200

    :sparkles: Add Opt-in form component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Accordion } from "../components"
+import { Accordion, OptForm } from "../components"
 import faqsData from "../fixtures/faqs.json"
 
 export function AccordionContainer() {
@@ -12,6 +12,14 @@ export function AccordionContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+      </OptForm>
     </Accordion>
+
   )
 }
