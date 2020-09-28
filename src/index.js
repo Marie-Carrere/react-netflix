96f0055bacec255887ace64e6255c7eda3cf5b6d
Marie-Carrere <marie.carrere@icloud.com>
2020-09-28 17:21:08 +0200

    :sparkles: Add context for Firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 import "normalize.css"
-import App from './App';
+import App from "./App"
 import { GlobalStyles } from "./global-styles"
+import { firebase } from "./lib/firebase-prod"
+import { FirebaseContext } from "./context/firebase"
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
