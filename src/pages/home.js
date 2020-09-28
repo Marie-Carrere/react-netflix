37139e441b7517b55994db2e778d897d953ed961
Marie-Carrere <marie.carrere@icloud.com>
2020-09-28 16:38:44 +0200

    :sparkles: Add Feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,24 +3,27 @@ import { AccordionContainer } from "../containers/faqs"
 import { FooterContainer } from "../containers/footer"
 import { HeaderContainer } from "../containers/header"
 import { JumbotronContainer } from "../containers/jumbotron"
-import { OptForm } from "../components"
+import { Feature, OptForm } from "../components"
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
-          <OptForm.Input placeholder="Email address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-        </OptForm>
+        <Feature>
+          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <AccordionContainer />
       <FooterContainer />
     </>
-
   )
 }
 
