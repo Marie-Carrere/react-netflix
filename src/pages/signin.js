ea4a070a4b1077d68cf05e10cf62466b7a9253f1
Marie-Carrere <marie.carrere@icloud.com>
2020-09-29 11:03:23 +0200

    :sparkles: Add Signin page & firebase authentication

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,71 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { useHistory } from "react-router-dom"
+import { FirebaseContext } from "../context/firebase"
+import { FooterContainer } from "../containers/footer"
+import { HeaderContainer } from "../containers/header"
+import { Form } from "../components"
+import * as ROUTES from "../constants/routes"
+
 
 export default function Signin() {
-  return <p>Hello from the signin</p>
+  const { firebase } = useContext(FirebaseContext)
+  const history = useHistory()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const isInvalid = password === "" || email === ""
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmail("");
+        setPassword("");
+        setError(error.message);
+      })
+  }
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  )
 }
